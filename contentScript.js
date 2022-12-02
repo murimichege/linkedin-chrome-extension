4933dddc846b777e4dcf890b8d35cdcfb97e7c2a
murimichege <chegeeddie@gmail.com>
2022-12-02 18:52:06 +0300

    code cleanup

--- contentScript.js
+++ contentScript.js
@@ -1,44 +1,36 @@
-const results = chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if(sender){
-        console.log(sender)   
-        console.log(request)
-    }
-    sendResponse({working:"working"})
-
-    return sender
-})
-setupChart(results)
-
-function setupChart(results){
-
-            return new Chart(document.getElementById("barchart").getContext('2d'), {
-    
-                type: 'bar',
-                data: {
-                    labels: ["Connections", "Messages", "Followers"],
-                    datasets: [
-                        {
-                            label: "User Info",
-                            backgroundColor: ["#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
-                            data: Object.keys(results || {})
-                        },
-                    ]
-                },
-                options: {
-                    legend: { display: true },
-                    title: {
-                        display: true,
-                        text: 'Your LinkedIn Metrics'
-                    }
-                }
-            });
-    
+const results = chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message) {
+      console.log('From the content script', message);
+      console.log(sender);
+      console.log(message);
     }
   
-
- 
-
-
- 
-
-
+    sendResponse({ working: 'working' });
+  
+    setupChart(message.metrics);
+    //   return sender;
+  });
+  
+  function setupChart(results) {
+    return new Chart(document.getElementById('barchart').getContext('2d'), {
+      type: 'bar',
+      data: {
+        labels: ['Connections', 'Messages', 'Followers'],
+        datasets: [
+          {
+            label: 'User Info',
+            backgroundColor: ['#8e5ea2', '#3cba9f', '#e8c3b9', '#c45850'],
+            data: Object.values(results || {}),
+          },
+        ],
+      },
+      options: {
+        legend: { display: true },
+        title: {
+          display: true,
+          text: 'Your LinkedIn Metrics',
+        },
+      },
+    });
+  }
+  
