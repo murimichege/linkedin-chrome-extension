e76b7ca3927f34558bb207ba3cf7ec4281456094
murimichege <chegeeddie@gmail.com>
2022-12-02 16:52:15 +0300

    changed naming for readability

--- contentScript.js
+++ contentScript.js
@@ -1,33 +1,44 @@
-const metrics = chrome.runtime.onMessage.addListener((sender, sendResponse, message) => {
-    console.log(sender)
+const results = chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if(sender){
+        console.log(sender)   
+        console.log(request)
+    }
+    sendResponse({working:"working"})
+
     return sender
 })
-setupChart(metrics)
-function setupChart(metrics){
+setupChart(results)
 
-    return new Chart(document.getElementById("barchart").getContext('2d'), {
+function setupChart(results){
 
-    type: 'bar',
-    data: {
-        labels: ["Connections", "Messages", "Followers"],
-        datasets: [
-            {
-                label: "User Info",
-                backgroundColor: ["#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
-                data: Object.keys(metrics || {})
-            },
-        ]
-    },
-    options: {
-        legend: { display: true },
-        title: {
-            display: true,
-            text: 'Your LinkedIn Metrics'
-        }
+            return new Chart(document.getElementById("barchart").getContext('2d'), {
+    
+                type: 'bar',
+                data: {
+                    labels: ["Connections", "Messages", "Followers"],
+                    datasets: [
+                        {
+                            label: "User Info",
+                            backgroundColor: ["#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
+                            data: Object.keys(results || {})
+                        },
+                    ]
+                },
+                options: {
+                    legend: { display: true },
+                    title: {
+                        display: true,
+                        text: 'Your LinkedIn Metrics'
+                    }
+                }
+            });
+    
     }
-});
+  
 
-}
+ 
+
+
  
 
 
